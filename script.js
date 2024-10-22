147033f7931d8b84d8ef200b5aa0667551a5a5b3
Maria Eduarda Alves de Melo <dudaworkm@gmail.com>
2024-10-21 21:58:26 -0300

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list")
 
+//função para adicionar tarefa
 function addTask(){
     if(inputBox.value === ''){
-        alert("Por favor, digite a sua tarefa")
+        alert("Por favor, digite a sua tarefa") //validação de caracteres
     }
     else{
         let li = document.createElement("li")
         li.innerHTML = inputBox.value
         listContainer.appendChild(li)
         let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
+        span.innerHTML = "\u00d7" //elemento html que representa o "X" de remover
         li.appendChild(span)
     }
     inputBox.value = ""
     salvarDados()
 }
+// função para remover tarefas
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
@@ -25,6 +27,7 @@ listContainer.addEventListener("click", function(e){
     }
 }, false)
 
+//funções para exibir e salvar dados
 function salvarDados(){
     localStorage.setItem("data", listContainer.innerHTML)
 }
